f14341db8fcf18e221682f14eb12314870264b42
Aashish-png <aashishsarwa512@gmail.com>
2022-11-06 03:22:35 +0530

    made navbar

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './nav.css'
-const nav = () => {
-  return (
-    <div>nav</div>
-  )
+import {BiHomeHeart}from'react-icons/bi'
+import {BiUserCheck}from'react-icons/bi'
+import {AiOutlineBook}from'react-icons/ai'
+import {MdOutlineHomeRepairService}from'react-icons/md'
+import {MdContactPhone}from'react-icons/md'
+import { useState } from 'react'
+
+const Nav = () => {
+  const [activeNav,setActiveNav]= useState('#')
+ return(
+  <nav>
+  <a href="#" onClick={()=>setActiveNav('#')} className={activeNav =='#'?'active':''}><BiHomeHeart/></a>
+  <a href="#about" onClick={()=>setActiveNav('#about')} className={activeNav =='#about'?'active':''}><BiUserCheck/></a>
+  <a href="#experience" onClick={()=>setActiveNav('#experience')} className={activeNav =='#experience'?'active':''}><AiOutlineBook/></a>
+  <a href="#services" onClick={()=>setActiveNav('#services')} className={activeNav =='#services'?'active':''}><MdOutlineHomeRepairService/></a>
+  <a href="#contact" onClick={()=>setActiveNav('#contact')} className={activeNav =='#contact'?'active':''}><MdContactPhone/></a>
+  </nav>
+ )
 }
 
-export default nav
+export default Nav
