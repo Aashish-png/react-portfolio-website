bd7a06dc8ea23d3143372b5ac6afefa3cec46008
Aashish-png <aashishsarwa512@gmail.com>
2022-11-06 04:42:42 +0530

    added about me section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {BsAward} from 'react-icons/bs'
+import {FaUsers} from 'react-icons/fa'
+import {AiFillProject} from 'react-icons/ai'
 const about = () => {
   return (
-    <section id='about'>about</section>
+    <section id='about'>
+      <h5>Get to Know </h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About me "/>
+          </div>
+        </div>
+        <div className="about__content">
+            <div className="about__cards">
+              <article  className='about__card'>
+                <BsAward className='about__icon'/>
+                <h5>Experience</h5>
+                <small>6+ months working </small>
+              </article>
+              {/* <article  className='about__card'>
+                <FaUsers className='about__icon'/>
+                <h5>Clients</h5>
+                <small>2+ </small>
+              </article> */}
+              <article  className='about__card'>
+                <AiFillProject className='about__icon'/>
+                <h5>Projects</h5>
+                <small>3+ completed</small>
+              </article>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus labore accusantium tempore eligendi soluta qui minima sapiente ratione repellat cumque sit, quaerat ducimus commodi blanditiis explicabo rerum nesciunt voluptatem fugiat.</p>
+            <a href="#contact"  className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
