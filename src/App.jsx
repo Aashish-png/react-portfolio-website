e4507de30761f1dd19e51ab0bcb47f927488513c
Aashish-png <aashishsarwa512@gmail.com>
2022-11-06 01:58:32 +0530

    added css and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about';
+import Portfolio from './components/portfolio/portfolio'
+import Experience from './components/experience/Experience'
+import Services from './components/services/services'
+import Testimonial from './components/testimonials/testimonials'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
+
 
 const App = () => {
   return (
-    <div>App</div>
+   <>
+    <Header/>
+    <Nav/>
+    <About/>
+    <Experience/>
+    <Services/>
+    <Portfolio/>
+    <Testimonial/>
+    <Contact/>
+    <Footer/>
+   </>
   )
 }
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocial  from './HeaderSocial'
 const header = () => {
   return (
-    <div>header</div>
+   <header>
+    <div className="container header__container">
+      <h5>Hello I'm </h5>
+      <h1>Aashish Sarva</h1>
+      <h5 className="text-light">FullStack Developer</h5>
+      <CTA/>
+      <HeaderSocial/>
+      <div className="me">
+        <img  src={ME} alt="me"></img>
+      </div>
+      <a href="#contact" className='scroll__down'>Scroll Down</a>
+    </div>
+   </header>
   )
 }
 
